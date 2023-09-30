3650a9a27a50dc7e757c661ea6648814c78380d1
Liaksej <tarusov.liaksej@gmail.com>
2023-09-30 19:36:35 +0200

    Simplify Photo component by removing Image styled-component
    This commit aims to streamline the Photo component by removing the redundant Image styled-component. After refactoring the component, the image display and the delete functionality remained encapsulated within the IconButton. This makes our component more readable and maintainable, facilitating future modifications.

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -3,14 +3,6 @@ import { IconButton, Paper } from "@mui/material";
 import Cancel from "@mui/icons-material/Cancel";
 import { Dispatch, MouseEvent } from "react";
 
-const Image = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  height: 60,
-  lineHeight: "60px",
-}));
-
 interface Action {
   type: "addPhoto" | "deletePhoto";
   payload: { id: string; src?: string };
@@ -32,13 +24,11 @@ export const Photo = ({ photo, dispatch }: PhotoProps) => {
   };
 
   return (
-    <div>
-      <Image elevation={2} className="rel">
-        <IconButton className="abs" aria-label="delete" onClick={deleteHandler}>
-          <Cancel color="disabled" />
-        </IconButton>
-        <img src={photo.src} alt={photo.id} width="300" />
-      </Image>
+    <div className="rel">
+      <IconButton className="abs" aria-label="delete" onClick={deleteHandler}>
+        <Cancel color="disabled" />
+      </IconButton>
+      <img src={photo.src} alt={photo.id} width="300" />
     </div>
   );
 };
